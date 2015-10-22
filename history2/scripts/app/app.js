185fbf675432545b48b2bf0b19a2cfa6c5a8a7d0
giles <giles.bradshaw@sigyl.com>
2015-10-22 22:28:34 +0100

    Initial commit

---
+++ history2/scripts/app/app.js
@@ -0,0 +1,32 @@
+﻿require.config({
+    paths: {
+        'knockout': '/scripts/knockout-3.3.0',
+        'punches' : '/scripts/knockout.punches',
+        'jquery': '/scripts/jquery-2.1.4.min',
+        'text': '/scripts/text',
+        'pager': '/scripts/pager',
+        'history': '/scripts/history.js/history',
+        'domReady': '/scripts/domReady',
+        'history.adapter.jquery': '/scripts/history.js/history.adapter.jquery'
+        
+    },
+    shim: {
+        'pager': ['jquery', 'knockout'],
+        'history.adapter.jquery': ['jquery'],
+        'history': ['jquery', 'pager', 'knockout', 'history.adapter.jquery']
+    },
+    waitSeconds: 0
+});
+
+require(['knockout', 'jquery', 'pager', 'history', 'punches', 'domReady!'], function (ko, $, pager) {
+
+    ko.punches.enableAll();
+    pager.useHTML5history = true;
+    pager.Href5.history = History;
+    var viewModel = {
+        giles: 'giles'
+    };
+    pager.extendWithPage(viewModel);
+    ko.applyBindings(viewModel);
+    pager.startHistoryJs();
+});
